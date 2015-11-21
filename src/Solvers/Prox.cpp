d3fafe69d0065241e48fcf0bb557d9e8f496692e
ChrKroer <chrkroer@gmail.com>
2015-11-20 19:42:15 -0500

    added prox class.

---
+++ src/Solvers/Prox.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Christian Kroer on 11/20/15.
+//
+
+#include "Prox.h"
---
+++ src/Solvers/Prox.h
@@ -0,0 +1,15 @@
+//
+// Created by Christian Kroer on 11/20/15.
+//
+
+#ifndef EFG_SOLVE_PROX_H
+#define EFG_SOLVE_PROX_H
+
+
+class Prox {
+public:
+  void Step(double[] realization_plan, )
+};
+
+
+#endif //EFG_SOLVE_PROX_H
